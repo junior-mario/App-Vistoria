9827cfa51d8c9f68ab4a1a940f11b65f78c6feff
junior-mario <mario.junior@live.com>
2025-06-27 15:27:59 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+
+import NetlifyTest from './NetlifyTest'; // Importa nosso novo componente
+
+function App() {
+  return (
+    <div className="App">
+      <NetlifyTest />
+    </div>
+  );
+}
+
+export default App;
